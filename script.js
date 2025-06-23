2ed2cbda124ff0c9d73c1edf1d717bf3c6c76c67
ohomen <133672224+ohomen@users.noreply.github.com>
2025-06-23 12:27:49 -0300

    m

--- script.js
+++ script.js
@@ -6,63 +6,62 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-        alternativas: [
+        enunciado: " A procrastinação é apenas uma falta de disciplina."
+        alternativas: [""
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto:  "Verdade, quem procrastina é só porque não tem disciplina suficiente."
+                afirmacao: "Falso, a procrastinação pode ser um reflexo de fatores emocionais, como medo ou ansiedade."
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Verdade, quem não tem uma rotina rígida nunca vai ser produtivo.",
+                afirmacao: " Falso, a procrastinação pode ser um reflexo de fatores emocionais, como medo ou ansiedade."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
-        alternativas: [
+        enunciado: "Estabelecer uma rotina rígida é a única forma de ser produtivo."
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Verdade, quem não tem uma rotina rígida nunca vai ser produtivo."
+                afirmacao: " Falso, uma rotina flexível, mas bem planejada, também pode ser eficaz."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: " Falso, uma rotina flexível, mas bem planejada, também pode ser eficaz.",
+                afirmacao: " Falso, qualquer objetivo vago pode ser alcançado sem uma meta específica"
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Ter uma meta clara e específica é fundamental para alcançar um objetivo."
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Verdade, sem metas claras, fica difícil saber para onde estamos indo."
+                afirmacao: "Falso, qualquer objetivo vago pode ser alcançado sem uma meta específica."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Verdade, sem metas claras, fica difícil saber para onde estamos indo.",
+                afirmacao: " Falso, produtividade é sobre qualidade e foco, não quantidade de horas trabalhadas."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "A produtividade só depende de trabalhar mais horas por dia.",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: " Verdade, trabalhar mais horas sempre aumenta a produtividade",
+                afirmacao: "Falso, produtividade é sobre qualidade e foco, não quantidade de horas trabalhadas."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Verdade, trabalhar mais horas sempre aumenta a produtividade",
+                afirmacao: " Falso, pausas atrapalham o fluxo de trabalho e diminuem a produtividade."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: " Falso, pausas atrapalham o fluxo de trabalho e diminuem a produtividade.",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: " Falso, pausas atrapalham o fluxo de trabalho e diminuem a produtividade.",
+                afirmacao: " Verdade, pausas ajudam a manter o foco e a energia."
             },
             {
                 texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
